b6ef29349c92ef3381feb7ed1544b73f4a671c2c
fengweibin <tx_xinxin2006@163.com>
2017-12-29 10:21:14 +0800

    硬件管理

---
+++ hardware-parent/monitor-hardware-api/src/main/java/com.kalix.monitor.hardware.api/dao/IHardwareInfoBeanDao.java
@@ -0,0 +1,11 @@
+package com.kalix.monitor.hardware.api.dao;
+
+import com.kalix.framework.core.api.dao.IGenericDao;
+import com.kalix.monitor.hardware.entities.HardwareInfoBean;
+
+/**
+ * Created by Administrator on 2017/3/3.
+ */
+public interface IHardwareInfoBeanDao extends IGenericDao<HardwareInfoBean,Long> {
+
+}
---
+++ hardware-parent/monitor-hardware-api/src/main/java/com.kalix.monitor.hardware.api/dao/IHardwareLogBeanDao.java
@@ -0,0 +1,12 @@
+package com.kalix.monitor.hardware.api.dao;
+
+import com.kalix.framework.core.api.dao.IGenericDao;
+import com.kalix.monitor.hardware.entities.HardwareInfoBean;
+import com.kalix.monitor.hardware.entities.HardwareLogBean;
+
+/**
+ * Created by Administrator on 2017/3/3.
+ */
+public interface IHardwareLogBeanDao extends IGenericDao<HardwareLogBean,Long> {
+
+}
