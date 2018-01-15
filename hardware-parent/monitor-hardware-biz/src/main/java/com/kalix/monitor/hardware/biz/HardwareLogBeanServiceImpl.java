16a852eae2cf7e688e8808f22d448bd668b20f4b
fengweibin <tx_xinxin2006@163.com>
2018-01-15 16:03:09 +0800

    修改方法注入framework-core-config服务

--- hardware-parent/monitor-hardware-biz/src/main/java/com/kalix/monitor/hardware/biz/HardwareLogBeanServiceImpl.java
+++ hardware-parent/monitor-hardware-biz/src/main/java/com/kalix/monitor/hardware/biz/HardwareLogBeanServiceImpl.java
@@ -2,6 +2,7 @@ package com.kalix.monitor.hardware.biz;
 
 
 import com.google.gson.*;
+import com.kalix.framework.core.api.config.IAdminConfigService;
 import com.kalix.framework.core.api.persistence.JsonData;
 import com.kalix.framework.core.api.persistence.JsonStatus;
 import com.kalix.framework.core.impl.biz.ShiroGenericBizServiceImpl;
@@ -19,7 +20,11 @@ import java.util.*;
 public class HardwareLogBeanServiceImpl extends ShiroGenericBizServiceImpl<IHardwareLogBeanDao, HardwareLogBean> implements IHardwareLogBeanService {
 
     private JsonStatus jsonStatus = new JsonStatus();
+    private IAdminConfigService adminConfigService;
 
+    public void setAdminConfigService(IAdminConfigService adminConfigService) {
+        this.adminConfigService = adminConfigService;
+    }
 
     /**
      * 获取邮箱配置信息
@@ -27,39 +32,8 @@ public class HardwareLogBeanServiceImpl extends ShiroGenericBizServiceImpl<IHard
      * @return
      */
     public JsonData getHardwareMailInfo() {
-        JsonData jsondata = new JsonData();
-        Dictionary<String, Object> config=ConfigUtil.getAllConfig("config.monitor.config");
-
-        HashMap map = new HashMap();
-        Enumeration enumeration= config.keys();
-        List list = new ArrayList();
-        String key_str="";
-        for(Enumeration e=enumeration;e.hasMoreElements();){
-            String keyName=e.nextElement().toString();
-
-            if(!"felix.fileinstall.filename".equals(keyName)&&!"service.pid".equals(keyName))
-            {
-                String key=keyName.split("\\.")[0];
-                String value=keyName.split("\\.")[1];
-                if(key_str.indexOf(key)>-1)
-                {
-                    map.put(value,config.get(keyName));
-
-                }
-                else
-                {
-                    Map map_parent = new HashMap();
-                    map = new HashMap();
-                    map.put(value,config.get(keyName));
-                    map_parent.put(key,map);
-                    list.add(map_parent);
-                }
-                key_str+=key+",";
-            }
-
-        }
+        JsonData  jsondata= adminConfigService.getConfigInfo("config.monitor.config");
         jsonStatus.setSuccess(true);
-        jsondata.setData(list);
         return jsondata;
     }
 
@@ -70,19 +44,7 @@ public class HardwareLogBeanServiceImpl extends ShiroGenericBizServiceImpl<IHard
      */
     public  JsonStatus configureHardwareMail(String content)
     {
-
-        Dictionary<String, Object> config=ConfigUtil.getAllConfig("config.monitor.config");
-        JsonParser jsonParser = new JsonParser();
-        JsonElement el= jsonParser.parse(content);
-        JsonObject jobj=el.getAsJsonObject();
-        Set<Map.Entry<String, JsonElement>> entrySet=jobj.entrySet();
-        for (Map.Entry<String, JsonElement> entry : entrySet) {
-            JsonObject jObject = jobj.getAsJsonObject(entry.getKey());
-            String id=jObject.getAsJsonPrimitive("id").getAsString();
-            String value=jObject.getAsJsonPrimitive("value").getAsString();
-            config.put(id+".value",value);
-        }
-        ConfigUtil.saveAllConfig(config,"config.monitor.config");
+        adminConfigService.configureConfigInfo(content,"config.monitor.config");
         jsonStatus.setMsg("设置成功！");
         jsonStatus.setSuccess(true);
         return jsonStatus;
